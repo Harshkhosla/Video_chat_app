8160b0bc8b8b5e4bfafca7cb2eeeccd8553e4566
harshkhosla <harsh.khosla@netomi.com>
2025-11-04 16:03:07 +0530

    adding the initial video acess allowing data

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,33 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [link, setLink] = useState<string>('')
+  const localVideoRef = useRef<HTMLVideoElement | null>(null)
+  const localStreamRef = useRef<MediaStream | null>(null)
+
+
+  const handleChnage = (e:any)=>{
+  setLink(e.target.value)
+  }
+  const handleSubmit =()=>{
+
+  }
+
+  useEffect(()=>{
+    async function setupLocalMedia(){
+      try {
+    const stream = await navigator.mediaDevices.getUserMedia({ video: true, audio: true });
+    console.log(stream, 'Failed')
+     localStreamRef.current = stream
+     console.log(localStreamRef,'localStreamRef')
+      if (localVideoRef.current) localVideoRef.current.srcObject = stream
+     console.log(localVideoRef)
+      } catch (err) {
+        console.error('Failed to get user media', err)
+        // show UI message in production
+      }
+    }
+    setupLocalMedia()
+     return () => {
+      localStreamRef.current?.getTracks().forEach(t => t.stop())
+    }
+  },[])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <p className="ml-4 mt-10">
+        DO A video chat 
       </p>
+      <div className='flex justify-center'>
+      <input type="text" onChange={handleChnage} value={link} className='border ml-5'/>
+      <button className='px-3  py-1 border' onClick={handleSubmit}>Join </button>
+
+      <div className="flex justify-center mt-6">
+        <video
+          ref={localVideoRef}
+          autoPlay
+          playsInline
+          muted
+          className="w-80 h-56 bg-black"
+        />
+      </div>
+      </div>
     </>
   )
 }
